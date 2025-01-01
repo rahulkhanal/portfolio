b74677c1151bce3b62f63ca7e27ff65f1f7e395e
Rahul Khanal <92104751+rahulkhanal@users.noreply.github.com>
2025-01-01 13:57:50 +0545

    route established

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core'
+import { Center, Image, MantineProvider, Stack, Title } from '@mantine/core'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider theme={{
       fontFamily: "Anonymous Pro, monospace"
     }}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/message-to-sushhhbaby"
+            element={
+              <Stack h={'100vh'} align='center' justify='center'>
+                <Title align='center'>
+                  Sushhhbaby lai mero kiss hai 😘
+                </Title>
+                <Image w="300px" src="https://media.tenor.com/iFhAw2oC0XIAAAAj/slanted-kiss.gif" />
+              </Stack>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>,
 )
